7fcb68d6ba4d6aaf7e065a64d692ab4c69670c63
Augustine Theodore <16444359+augtheo@users.noreply.github.com>
2023-12-30 14:08:59 +0530

    Set global variable colors_name

--- lua/pywal/init.lua
+++ lua/pywal/init.lua
@@ -3,6 +3,7 @@ local core = require('pywal.core')
 local highlights = require('pywal.highlights')
 
 function M.setup ()
+  vim.g.colors_name = "pywal"
   local colors = core.get_colors()
   vim.opt.termguicolors = true
   highlights.highlight_all(colors)
